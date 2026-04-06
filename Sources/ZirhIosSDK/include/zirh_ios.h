91637889223347fbb5605cd401944a570cceed60
DevShoh <101915318+DevShoh@users.noreply.github.com>
2026-04-06 10:33:21 +0500

    Update zirh_ios.h

--- Sources/ZirhIosSDK/include/zirh_ios.h
+++ Sources/ZirhIosSDK/include/zirh_ios.h
@@ -3,8 +3,30 @@
 
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void zirh_ios_boshlash(const char* keyPath, const char* dataPath);
+
 char* ios_malumot_olish(const char* path);
+
 void ios_xotirani_tozalash(void* ptr);
+
+char* ios_malumot_almashish(
+    const char* url,
+    const char* method,
+    const char* body,
+    const char* headers,
+    const char* filePath,
+    const uint8_t* fileBytes,
+    int32_t bytesCount,
+    const char* fileName,
+    const char* fileField
+);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif

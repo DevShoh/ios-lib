4120997aa332d3e8dfd2e07cae05aa7cd8581477
DevShoh <101915318+DevShoh@users.noreply.github.com>
2026-04-06 09:26:38 +0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,16 +1,32 @@
+// swift-tools-version: 6.2
+import PackageDescription
+
 let package = Package(
     name: "ZirhIosSDK",
-    products: [
-        .library(name: "ZirhIosSDK", targets: ["ZirhIosSDKWrapper"])
+    platforms: [
+        .iOS(.v13) // SDK ishlaydigan eng minimal iOS versiyasini belgilang
     ],
+    products: [
+        .library(
+            name: "ZirhIosSDK",
+            targets: ["ZirhIosSDKWrapper"]
+        ),
+    ],
+    dependencies: [],
     targets: [
+        // 1. Binary Framework (.xcframework)
         .binaryTarget(
             name: "ZirhIosSDKBinary",
             path: "ZirhIosSDK.xcframework"
         ),
+        
+        // 2. Wrapper Target
+        // Bu target binary frameworkni loyihaga bog'lab beradi
         .target(
             name: "ZirhIosSDKWrapper",
-            dependencies: ["ZirhIosSDKBinary"],
+            dependencies: [
+                .target(name: "ZirhIosSDKBinary")
+            ],
             path: "Sources/ZirhIosSDK"
         )
     ]

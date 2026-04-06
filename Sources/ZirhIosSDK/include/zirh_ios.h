7fa8e65178d05920eb69a302a676cb13e5a62cef
DevShoh <101915318+DevShoh@users.noreply.github.com>
2026-04-06 10:27:56 +0500

    Create zirh_ios.h

---
+++ Sources/ZirhIosSDK/include/zirh_ios.h
@@ -0,0 +1,10 @@
+#ifndef ZIRH_IOS_H
+#define ZIRH_IOS_H
+
+#include <stdint.h>
+
+void zirh_ios_boshlash(const char* keyPath, const char* dataPath);
+char* ios_malumot_olish(const char* path);
+void ios_xotirani_tozalash(void* ptr);
+
+#endif
